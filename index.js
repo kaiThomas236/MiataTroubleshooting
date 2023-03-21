2bdc47eb6f09d29bce9e4237720851f191ee4028
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2023-03-21 13:34:56 -0600

    onload change url

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 var url = window.location.href;
+window.onload = welcomeFunc();
 
-// var id;
-// var welc = true;
-
-// var n = url.lastIndexOf('/');
-// if (url.substring(n) == "/index.html"){
-//     id = "welcome";
-// } else {
-//     var k = url.lastIndexOf('#');
-//     id = url.substring(k).substring(1);
-// }
-
-
-// var active = document.getElementById(id);
-// console.log(active.classList);
-// active.classList.add("section-active");
+function welcomeFunc () {
+    var n = url.lastIndexOf('/');
+    if (url.substring(n) == "/index.html") {
+        window.location.href = url + "#welcome";
+    }
+};
 
 var n = url.lastIndexOf('/');
-if (url.substring(n) == "/index.html"){
-    window.location.href = url+"#welcome";
+if (url.substring(n) == "/index.html") {
+    window.location.href = url + "#welcome";
 }
+
+
+
