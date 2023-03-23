1a47550ca52815138da6ab664fbdb5f61b8040e3
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2023-03-23 13:01:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ window.onload = welcomeFunc();
 var n = url.lastIndexOf('.');
 
 function welcomeFunc () {
-    if (url.substring(n) == ".com") {
+    if (url.substring(n) == "com") {
         window.location.href = url + "#welcome";
     }
 };
 
 
-if (url.substring(n) == ".com") {
+if (url.substring(n) == "com") {
     window.location.href = url + "#welcome";
 }
 
