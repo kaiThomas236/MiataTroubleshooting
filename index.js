54cf2969a0da49c09e8d52e891cbcd31b33c7cbb
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2023-03-23 13:15:42 -0600

    debugging, think I got it

--- index.js
+++ index.js
@@ -7,19 +7,19 @@
 
 // function welcomeFunc () {
 //     console.log(url.substring(n));
-//     if (url.substring(n) == ".com") {
+//     if (url.substring(n) == ".com" || url.substring(n) == ".com/") {
 //         console.log("in the if in the func")
 //         window.location.href = url + "#welcome";
 //     }
 // };
 
 
-// if (url.substring(n) == ".com") {
-//     console.log("in the if out the func")
-//     window.location.href = url + "#welcome";
-// }
+if (url.substring(n) == ".com" || url.substring(n) == ".com/") {
+    console.log("in the if out the func")
+    window.location.href = url + "#welcome";
+}
 
-window.location.href = window.location.href + "#test";
+// window.location.href = window.location.href + "#test";
 
 
 
