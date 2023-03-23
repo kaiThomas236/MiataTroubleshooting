6e2ffa7c0fe121594772a2c7d1997bbb59218dd7
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2023-03-23 13:04:03 -0600

    debugging

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var url = window.location.href;
 window.onload = welcomeFunc();
 var n = url.lastIndexOf('.');
+console.log(url.substring(n));
+
 
 function welcomeFunc () {
-    if (url.substring(n) == "com") {
+    console.log(url.substring(n));
+    if (url.substring(n) == ".com") {
+        console.log("in the if in the func")
         window.location.href = url + "#welcome";
     }
 };
 
 
-if (url.substring(n) == "com") {
+if (url.substring(n) == ".com") {
+    console.log("in the if out the func")
     window.location.href = url + "#welcome";
 }
 
